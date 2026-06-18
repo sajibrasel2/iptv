0439a3b7d70ad02dcac76985ce65596000681527
Sajibrasel <your-email@example.com>
2026-06-18 19:12:07 +0600

    Force update: sync ads.js with Monetag configuration

--- ads.js
+++ ads.js
@@ -1,1 +1,8 @@
-(function(s){s.dataset.zone='11167304';s.src='https://n6wxm.com/vignette.min.js';([document.documentElement, document.body].filter(Boolean).pop().appendChild(s))})(document.createElement('script'));
+<script>
+    (function () {
+    var s = document.createElement('script');
+    s.dataset.zone = '11167304';
+    s.src = 'https://n6wxm.com/vignette.min.js';
+    document.body.appendChild(s);
+})();
+</script>
